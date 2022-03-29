4887de2aba6c66ab490dafce8405ada45d3fd2ea
vaibhavMahmia <vaibhavwwe76@gmail.com>
2022-03-29 11:21:02 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -7,11 +7,12 @@ const port = process.env.PORT || 3000;
 const staticpath = path.join(__dirname, "../public");
 const templatepath = path.join(__dirname, "../templates/views");
 const partialpath = path.join(__dirname, "../templates/partials");
-//console.log(staticpath);
 
 app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")));
 app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")));
 app.use('/jq', express.static(path.join(__dirname, "../node_modules/jquery/dist")));
+app.use('/customcss', express.static(path.join(__dirname, "../public/css")));
+app.use('/icon', express.static(path.join(__dirname, "../images")));
 
 app.use(express.static(staticpath));
 app.set('view engine','hbs');
